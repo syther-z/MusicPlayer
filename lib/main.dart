c17a38d739183d72125d4b7f6a33c4549febdf12
Pawan Kumar <pawansharma9115078358@gmail.com>
2025-08-22 19:24:12 +0530

    added images

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,7 @@ class MyApp extends StatelessWidget {
         ],
          child:  SafeArea(
            child: MaterialApp.router(
+            debugShowCheckedModeBanner: false,
             routerConfig: Approuter().router,
            ),
          ),
